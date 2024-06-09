45c3e1fe24b7132decd681d000d1f419fedb58fc
Maria Eugenia Diaz <117225183+maeudi@users.noreply.github.com>
2024-06-09 16:02:25 +0000

    domingo 9-jun-24

---
+++ Ch2 - Basics/ch2quiz.py
@@ -0,0 +1,30 @@
+# thestr = "Ogres are often foolhardy oafs"
+# newstr = ""
+# for i, c in enumerate(thestr):
+#     if c == "o":
+#         continue
+#     if i > 20:
+#         break
+#     newstr += c
+# print(newstr)
+
+# value=input("2+2=")
+
+# def inc(a,b=1):
+#     return(a+b)
+# a=inc(1)
+# a=inc(a,a)
+# print(a)
+
+# thestr = "This is a string"
+# print(thestr)
+# thestr = 5
+
+# var="123456789"
+# print(var[1:6:2])
+
+# assume two integer variables, x and y
+try:
+    result = x / y
+except ZeroDivisionError:
+    print("A divide by zero error occurred")
